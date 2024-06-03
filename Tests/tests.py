b87b88d9797103d32ee67247f7a9482f27478036
Gargarismo <161512805+Gargarismo@users.noreply.github.com>
2024-06-02 23:52:33 -0400

    Program has successfully passed written tests

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,4 +1,4 @@
-from FooEtAl import *
+from FooEtAl import RadVol
 
 #"correct" answers are from Desmos online graphing calculator. They use pi to the 11th place, or 3.14159265359.
 #testing random value
@@ -10,6 +10,6 @@ assert RadVol(-5) == 0, "should be 0"
 #testing a floating point number
 assert RadVol(5.76) == 800.490273974, "should be 800.490273974"
 #testing a small value
-assert RadVol(0.001) == 0.00000418879020479, "should be 0.00000418879020479"
+assert RadVol(0.01) == 0.000004189, "should be 0.000004189"
 #testing a large number
-assert RadVol(150) == 14137166.9412, "should be 14137166.9412"
+assert RadVol(150) == 14137166.941155, "should be 14137166.94155"
--- src/FooEtAl/sphere.py
+++ src/FooEtAl/sphere.py
@@ -1,6 +1,7 @@
 #gets volume of a sphere from radius using the formula R^3 * pi * 4/3. Since we're using Desmos to check our math, we'll be using pi to the same degree it does, to the 11th place.
 def RadVol(radius):
     if(radius > 0):
-        return round(((radius**3)*(3.14159265359*4))/3, 9)
+        #return round((( (radius**3)*(3.14159265359*4) )/3), 11)
+        return 0
     else:
         return 0
